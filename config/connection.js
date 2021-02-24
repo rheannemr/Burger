ff6b542c602f7f66d910b809bf4f2dd75cb42d62
Rheanne Reyes <rheannemr@gmail.com>
2021-02-23 18:05:32 -0800

    Create connection to mysql

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'burgers_db',
+});
+
+// Connect to mysql database
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id: ${connection.threadId}`);
+});
+
+module.exports = connection;
