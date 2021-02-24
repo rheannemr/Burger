41735c8c0189a70cb74ac3f1934bffef93ff524b
Rheanne Reyes <rheannemr@gmail.com>
2021-02-23 17:55:13 -0800

    Update seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ('Bacon Cheeseburger')
+
+INSERT INTO burgers (burger_name)
+VALUES ('Veggie Burger')
+
+INSERT INTO burgers (burger_name)
+VALUES ('Crispy Chicken Burger')
