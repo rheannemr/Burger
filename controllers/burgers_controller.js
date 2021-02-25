94d25cef3df2d7d8cdec39b537c54986551980ba
Rheanne Reyes <rheannemr@gmail.com>
2021-02-25 11:37:09 -0800

    Create router

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const burger = require('../models/burger.js');
 const router = express.Router();
+
+router.get("/", (req, res) => {
+
+});
+
+router.post("/", (req, res) => {
+
+});
+
+router.put("/", (req, res) => {
+
+});
+
+module.exports = router;
