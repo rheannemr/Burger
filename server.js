eef07edd0d1203f7cf613eb1f6789686c46abc93
Rheanne Reyes <rheannemr@gmail.com>
2021-02-25 14:32:20 -0800

    Add PORT and app.listen

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+// app.use(routes);
+
+app.listen(PORT, () =>
+  console.log('Server is listening on http://localhost:' + PORT)
+);
