4d3e07ed83981c65627715d793f7b51ba0241734
Rheanne Reyes <rheannemr@gmail.com>
2021-02-25 14:05:20 -0800

    Update routers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,16 +2,23 @@ const express = require('express');
 const burger = require('../models/burger.js');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-
+router.get('/', (req, res) => {
+    burger.selectAll(function(data) {
+        var burgerObj = { burgers: data };
+        console.log(burgerObj);
+        res.render('index', burgerObj);
+    })
 });
 
-router.post("/", (req, res) => {
-
+router.post('/api/burgers', (req, res) => {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], (result) => {
+        res.json({ id: result.insertId });
+    });
 });
 
-router.put("/", (req, res) => {
-
+router.put('/api/burgers/:id', (req, res) => {
+    
+    burger.updateOne(req.params.id,)
 });
 
 module.exports = router;
