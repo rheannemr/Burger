f34e3671b0c19e5ef99a00e73ebde9f2ed7be015
Rheanne Reyes <rheannemr@gmail.com>
2021-05-02 19:30:06 -0700

    Update controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,5 @@
-const router = express.Router();
 const express = require('express');
+const router = express.Router();
 // Import the model (burger.js) to use its database functions
 const burger = require('../models/burger.js');
 
